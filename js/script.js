9c4478423c3089ffbcef91459215f06992d708cc
tomkdev17 <143559210+tomkdev17@users.noreply.github.com>
2023-09-12 12:41:34 -0500

    Fixed commas syntax error and swapped for single quotes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-	{name: "Bulbasaur", type: "grass", height: 0.7}
-	{name: "Charmander", type: "fire", height: 0.6}
-	{name: "Squirtle", type: "water", height: 0.5}
+	{name: 'Bulbasaur', type: ['grass', 'poison'], height: 0.7},
+	{name: 'Charmander', type: ['fire'], height: 0.6},
+	{name: 'Squirtle', type: ['water'], height: 0.5}
 	];
