b2209ac88540465ba87c7e0ab6d1ce2cada53305
tomkdev17 <143559210+tomkdev17@users.noreply.github.com>
2023-09-20 01:19:03 -0500

    Created for loop to print pokemon's names and sizes, added background color and font

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 let pokemonList = [
-	{name: 'Bulbasaur', type: ['grass', 'poison'], height: 0.7},
-	{name: 'Charmander', type: ['fire'], height: 0.6},
-	{name: 'Squirtle', type: ['water'], height: 0.5}
+
+	{
+		name: 'Bulbasaur', 
+		type: ['grass', 'poison'], 
+		height: 0.7
+	},
+
+	{
+		name: 'Charmander', 
+		type: ['fire'], 
+		height: 0.6
+	},
+
+	{
+		name: 'Squirtle', 
+		type: ['water'], 
+		height: 0.5
+	}
 	];
+
+for (let i = 0; i < pokemonList.length; i++) {
+
+	document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+
+	if (pokemonList[i].height > 0.6) {
+		document.write(' - Wow! That\'s a big Pokémon! ');
+	}
+	document.write('<br>');
+}
