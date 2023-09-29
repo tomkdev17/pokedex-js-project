818e34b0e4128c69d4755af1372236b9dfad3cd0
tomkdev17 <143559210+tomkdev17@users.noreply.github.com>
2023-09-29 04:41:21 -0500

    Made changes for exercise 1.6
    Changed the list of Pokémon to buttons that log the Pokémon info to the console when clicked. Added simple CSS queries to make collapse the grid when the screen size is smaller. Removed superfluous css file.

--- js/script.js
+++ js/script.js
@@ -25,20 +25,44 @@ let pokemonRepository = (function(){
 	function  getAll (){
 		return pokemonList;
 	}
-
+	//This function adds a pokemon to the repository
 	function add (pokemon){
+	 	if (typeof pokemon !== 'object'){
+	 		console.log('New Pokemon submission must be an item');
+	 	// still trying to figure out how to use Object.keys to validate the object keys
+	 	// } else if (Object.keys(pokemon) !== Object.keys(pokemonList[1]) ) {
+	 	// 	console.log('New Pokemon submission must contain "name, type, and height"');
+	 	} else {
 		pokemonList.push(pokemon);
+		}
 	}
+	//This function creates a button with the name of each pokemon 
+	function addListItem(pokemon){
+		let pokemonList = document.querySelector('.pokemon-list');
+		let listItem = document.createElement('li');
+		let button = document.createElement('button');
+
+		button.innerText = pokemon.name; 
+		button.classList.add('poke-button');
+		listItem.appendChild(button);
+		pokemonList.appendChild(listItem);
+
+		button.addEventListener('click', function(event){
+			showDetails(pokemon); 
+		})
+	} 
+	//This function logs the pokemon details to the console when the name is clicked (see above function)
+	function showDetails(pokemon){
+			console.log(pokemon);
+		}
+	
 	return {
 		getAll: getAll,
-		add: add 	
-	}
-})() 
-
+		add: add, 	
+		addListItem: addListItem
+	};
+})(); 
+// This prints the names of the pokemon to their respective buttons
 pokemonRepository.getAll().forEach(function (pokemon){
-	document.write(pokemon.name + ': [type: ' + pokemon.type + '] [height: ' + pokemon.height + ']');
-	if (pokemon.height > 0.6){
-		document.write(' Wow! That\'s a big Pokémon!');
-	}
-	document.write('</br>');
-})
+	pokemonRepository.addListItem(pokemon);
+});
